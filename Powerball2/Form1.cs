f9582f6e16dc45ff95b5fb98e03a02da6e45fcc2
yaaronson <yaaronso@student.touro.edu>
2023-11-05 18:25:55 -0500

    Pushing HW5 part 1

--- Powerball2/Form1.cs
+++ Powerball2/Form1.cs
@@ -11,27 +11,32 @@ namespace Powerball2
             InitializeUI();
         }
 
-        TextBox numInputBox;
+        TextBox[] numInputBox = new TextBox[6];
         Label displayResult;
 
         private void InitializeUI()
         {
             this.Text = "Powerball Number Checker";
-            this.Size = new System.Drawing.Size(600, 650);
+            this.Size = new System.Drawing.Size(650, 650);
 
-            // Label
+            
             Label instructions = new Label();
             instructions.Text = "Enter Numbers:";
             instructions.Location = new System.Drawing.Point(20, 20);
+            instructions.Size = new System.Drawing.Size(250, 30);
             this.Controls.Add(instructions);
 
-            // Number Input Box
-            numInputBox = new TextBox();
-            numInputBox.Location = new System.Drawing.Point(20, 50);
-            numInputBox.Size = new System.Drawing.Size(250, 30);
-            this.Controls.Add(numInputBox);
+            
+            for (int i = 0; i < 6; i++)
+            {
+                TextBox InputBox = new TextBox();
+                numInputBox[i] = InputBox;
+                InputBox.Location = new System.Drawing.Point(20 + i * 100, 50);
+                InputBox.Size = new System.Drawing.Size(80, 30); 
+                this.Controls.Add(InputBox);
+            }
 
-            // Check Numbers Button
+            
             Button checkButton = new Button();
             checkButton.Text = "Check Numbers";
             checkButton.Location = new System.Drawing.Point(20, 90);
@@ -40,25 +45,40 @@ namespace Powerball2
             this.Controls.Add(checkButton);
 
             displayResult = new Label();
-            displayResult.Size = new System.Drawing.Size(100, 68);
+            displayResult.Size = new System.Drawing.Size(100, 300);
             displayResult.Text = "  ";
             displayResult.Location = new System.Drawing.Point(120, 120);
             this.Controls.Add(displayResult);
-
         }
+
 
         int[] match = new int[6];
 
         private void checkButton_Click(object sender, EventArgs e)
         {
+            for (int i = 0; i < 6; i++) {
 
-            String convertNum = numInputBox.Text;
 
-            if (convertNum.Length != 6 || !int.TryParse(convertNum, out _))
-            {
-                displayResult.Text = "Invalid Formatting, ReDo Please";
-                return;
+                if (numInputBox[i].Text == null)
+                {
+                    displayResult.Text = "Invalid Formatting, ReDo Please";
+                    return;
+                }
+
+                if (numInputBox[i].Text.Length >= 2)
+                {
+                    displayResult.Text = "Only 1 digit per input box is allowed";
+                    return;
+                }
+
+                if (!int.TryParse(numInputBox[i].Text, out _))
+                {
+                    displayResult.Text = "Invalid Formatting, ReDo Please";
+                    return;
+                }
+
             }
+           
 
             Random numbers = new Random();
 
@@ -67,33 +87,37 @@ namespace Powerball2
                 match[i] = numbers.Next() % 10;
             }
 
-            if (CheckIfMatch(match) == true) {
+            if (CheckIfMatch(match) == true)
+            {
 
                 displayResult.Text = "Your Numbers are A Match!";
 
             }
-            else {
+            else
+            {
 
                 displayResult.Text = "Your Numbers Do NOT Match";
-                    
-             }
-        } 
 
-        private bool CheckIfMatch(int[] match) {
+            }
+        }
 
-                String convertNum = numInputBox.Text;
+        private bool CheckIfMatch(int[] match)
+        {
 
-                for (int i = 0; i < 6; i++)
+            for (int i = 0; i < 6; i++)
+            {
+                if (Int32.Parse(numInputBox[i].Text) == match[i])
                 {
-                    if (convertNum[i] == match[i])
-                    {
-                        
-                    }else { return false; }
 
                 }
+                else { return false; }
 
-                return true;
+            }
+
+            return true;
 
         }
     }
 }
+
+
